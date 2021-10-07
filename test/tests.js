00c7eccccb87b2947e83d3a5f2a50f414f421331
Philippe Ngom <philngom@gmail.com>
2021-10-07 11:15:20 -0700

    added add test and passed

--- test/tests.js
+++ test/tests.js
@@ -3,15 +3,21 @@
 
 const test = QUnit.test;
 
+function add(x, y) {
+    return x + y;
+}
+
 // name your test by what it is testing
 test('time to test a function', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
-    
-    //Act 
+    const x = 5;
+    const y = 3;
+    const expected = 8;
+
+    //Act
     // Call the function you're testing and set the result to a const
-    const actual = true; // use your function here
+    const actual = add(x, y); // use your function here
 
     //Expect
     // Make assertions about what is expected versus the actual result
